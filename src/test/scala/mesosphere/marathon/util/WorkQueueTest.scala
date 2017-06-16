071d9f06a0c40da8ad33f07a7f0fc679632c4e3d
Karsten Jeschkies <kjeschkies@mesosphere.com>
2017-06-16 16:58:23 +0200

    Stress test work queue.

--- src/test/scala/mesosphere/marathon/util/WorkQueueTest.scala
+++ src/test/scala/mesosphere/marathon/util/WorkQueueTest.scala
@@ -6,11 +6,17 @@ import java.util.concurrent.{ CountDownLatch, Semaphore }
 
 import mesosphere.UnitTest
 import mesosphere.marathon.core.async.ExecutionContexts.global
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{ Minutes, Span }
 
 import scala.concurrent.duration._
 import scala.concurrent.{ ExecutionContext, Future }
 
-class WorkQueueTest extends UnitTest {
+class WorkQueueTest extends UnitTest with Eventually {
+
+  override val timeLimit = Span(10, Minutes)
+  override implicit lazy val patienceConfig: PatienceConfig = PatienceConfig(timeout = Span(10, Minutes))
+
   "WorkQueue" should {
     "cap the maximum number of concurrent operations" in {
       val queue = WorkQueue("test", maxConcurrent = 1, maxQueueLength = Int.MaxValue)
@@ -28,6 +34,7 @@ class WorkQueueTest extends UnitTest {
       blocked.futureValue should be(1)
       counter.get() should equal(1)
     }
+
     "complete the future with a failure if the queue is capped" in {
       val queue = WorkQueue("abc", maxConcurrent = 1, maxQueueLength = 0)
       val semaphore = new Semaphore(0)
@@ -42,6 +49,7 @@ class WorkQueueTest extends UnitTest {
       }
 
     }
+
     "continue executing even when the previous job failed" in {
       val queue = WorkQueue("failures", 1, Int.MaxValue)
       queue.blocking {
@@ -51,16 +59,7 @@ class WorkQueueTest extends UnitTest {
         7
       }.futureValue should be(7)
     }
-    "defer to the parent queue when defined" in {
-      val parent = new WorkQueue("parent", 1, 1) {
-        override def apply[T](f: => Future[T])(implicit ctx: ExecutionContext): Future[T] =
-          Future.successful[T](1.asInstanceOf[T])
-      }
-      val queue = WorkQueue("child", 1, 1, Some(parent))
-      queue.blocking {
-        7
-      }.futureValue should equal(1)
-    }
+
     "run all tasks asked" in {
       val queue = WorkQueue("huge", 1, Int.MaxValue)
       val counter = new AtomicInteger()
@@ -73,6 +72,37 @@ class WorkQueueTest extends UnitTest {
       }
       latch.await()
       counter.get() should equal (100)
+    }
+
+    "synchronize and run all tasks" in {
+      val queue = WorkQueue("huge", 1, Int.MaxValue)
+      val counter = new AtomicInteger()
+
+      val numProcessors = Runtime.getRuntime.availableProcessors() / 4
+      val workItems: Int = 1000000
+      val latch = new CountDownLatch(numProcessors)
+      (1 to numProcessors).foreach { p =>
+        Future {
+
+          println(s"$p out of $numProcessors")
+
+          // All futures should start at the same time
+          latch.countDown()
+          latch.await()
+
+          (1 to workItems).foreach { i =>
+            queue(Future {
+              if (i % 100 == 0) {
+                println(s"Processing $p - $i")
+              }
+              counter.incrementAndGet()
+            })
+          }
+        }
+      }
+      eventually {
+        counter.get() should equal (workItems * numProcessors)
+      }
     }
   }
   "KeyedLock" should {
