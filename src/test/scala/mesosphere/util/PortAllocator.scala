a0634cae3e4a877a320d7a77e6b567698a62e235
Aleksey Dukhovniy <adukhovniy@mesosphere.io>
2017-06-26 15:45:49 +0200

    An attempt to fix AppDeploy and other Intgration Tests
    (cherry picked from commit b0ebe0e84ca7824a39e85a8006c194c8c109ea4c)

--- src/test/scala/mesosphere/util/PortAllocator.scala
+++ src/test/scala/mesosphere/util/PortAllocator.scala
@@ -1,12 +1,43 @@
 package mesosphere.util
 
 import java.net.ServerSocket
+import java.util.concurrent.atomic.AtomicInteger
 
-object PortAllocator {
-  def ephemeralPort(): Int = {
-    val socket = new ServerSocket(0)
-    val port = socket.getLocalPort
-    socket.close()
-    port
+import com.typesafe.scalalogging.StrictLogging
+
+import scala.annotation.tailrec
+import scala.util.{ Failure, Success, Try }
+import scala.util.control.NonFatal
+
+object PortAllocator extends StrictLogging {
+
+  //The Internet Assigned Numbers Authority (IANA) suggests the range 49152 to 65535
+  val PORT_MAX = 65535
+  val portCounter: AtomicInteger = new AtomicInteger(49152)
+
+  @tailrec
+  def freeSocket(): ServerSocket = {
+    val port = portCounter.incrementAndGet()
+    if (port > PORT_MAX) throw new RuntimeException("Out of ephemeral ports. Sorry")
+    Try(new ServerSocket(port)) match {
+      case Success(v) => v
+      case Failure(ex) =>
+        logger.warn(s"Failed to provide an ephemeral port because of ${ex.getMessage}. Will retry again...")
+        freeSocket()
+    }
   }
+
+  def closeSocket(socket: ServerSocket) = {
+    try {
+      socket.close()
+    } catch { case NonFatal(ex) => logger.debug(s"Failed to close port allocator's socket because ${ex.getMessage}") }
+  }
+
+  def ephemeralPort(): Int = {
+
+    val socket = freeSocket()
+    closeSocket(socket)
+    return socket.getLocalPort
+  }
+
 }
