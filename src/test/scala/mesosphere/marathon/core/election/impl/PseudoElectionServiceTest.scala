aa9eb37969404d0cc26d7cfd8849ccf78ce033d2
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2017-07-07 15:43:48 -0700

    Add ExitDisabledTest to the list of ancestors of PseudoElectionServiceTest

--- src/test/scala/mesosphere/marathon/core/election/impl/PseudoElectionServiceTest.scala
+++ src/test/scala/mesosphere/marathon/core/election/impl/PseudoElectionServiceTest.scala
@@ -5,15 +5,16 @@ package core.election.impl
 import akka.event.EventStream
 import mesosphere.AkkaUnitTest
 import mesosphere.chaos.http.HttpConf
-import mesosphere.marathon.core.base.{ LifecycleState, RichRuntime }
-import mesosphere.marathon.core.election.{ ElectionCandidate, ElectionService, LocalLeadershipEvent }
+import mesosphere.marathon.core.base.{LifecycleState, RichRuntime}
+import mesosphere.marathon.core.election.{ElectionCandidate, ElectionService, LocalLeadershipEvent}
+import mesosphere.marathon.test.ExitDisabledTest
 import org.mockito.Mockito
 import org.mockito.invocation.InvocationOnMock
 import org.mockito.stubbing.Answer
 import org.scalatest.concurrent.Eventually
-import org.scalatest.time.{ Seconds, Span }
+import org.scalatest.time.{Seconds, Span}
 
-class PseudoElectionServiceTest extends AkkaUnitTest with Eventually {
+class PseudoElectionServiceTest extends AkkaUnitTest with Eventually with ExitDisabledTest {
   override implicit lazy val patienceConfig: PatienceConfig = PatienceConfig(timeout = Span(10, Seconds))
 
   class Fixture {
