7ddc18e28a9eefea51af79da636db2a6739a41ba
Johannes Unterstein <junterstein@mesosphere.io>
2017-07-21 11:06:58 +0200

    feedback

--- ami/install.bash
+++ ami/install.bash
@@ -24,7 +24,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E56151BF && \
 
   apt-get -y update
 
-# Add github.com to knonw hosts
+# Add github.com to known hosts
 ssh-keyscan github.com >> /home/admin/.ssh/known_hosts
 ssh-keyscan github.com >> /root/.ssh/known_hosts
 
